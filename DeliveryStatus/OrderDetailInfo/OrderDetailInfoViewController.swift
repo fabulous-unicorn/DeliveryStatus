6da4dcad1be45ad2042b0ff20e920dd4d6a756f8
Alesya Volosach <alesya.volosach@gmail.com>
2022-06-16 13:42:29 +0300

    feat: Добавленна мок для таблицы

--- DeliveryStatus/OrderDetailInfo/OrderDetailInfoViewController.swift
+++ DeliveryStatus/OrderDetailInfo/OrderDetailInfoViewController.swift
@@ -15,26 +15,18 @@ class OrderDetailInfoViewController: UITableViewController {
         static let serviceCellIdentifier = "orderDetailsInfoServiceCell"
         static let parcelInfocellIdentifier = "orderDetailsInfoParcelInfoCell"
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(UINib(nibName: "OrderDetailsInfoTitleGroupCell", bundle: nil), forCellReuseIdentifier: Constants.titleGroupCellIdentifier)
-
-        
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return mainModel.groups.count - 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,13 +35,13 @@ class OrderDetailInfoViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.titleGroupCellIdentifier, for: indexPath) as? OrderDetailsInfoTitleGroupCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.titleGroupCellIdentifier, for: indexPath) as! OrderDetailsInfoTitleGroupCell
 
-        cell?.configure(title: "Откуда")
+        cell.configure(title: "Откуда")
         
         // Configure the cell...
 
-        return cell!
+        return cell
     }
 
     /*
@@ -97,4 +89,103 @@ class OrderDetailInfoViewController: UITableViewController {
     }
     */
 
+    
+    // MARK: - for test
+    let mainModel = OrderDetailsInfoViewModel(
+        title: "Посылочка, размер S",
+        description: "Супер-экспресс, доставит курьер до 16:00",
+        groups: [
+            .actor(
+                role: .sender,
+                items: [
+                    OrderDetailsInfoViewModel.OrderActor(
+                        title: "Москва, ул. Ленина 234, кв. 56",
+                        icon: UIImage(named: "orderDetailInfo.mapPin")!,
+                        behavior: .copy
+                    ),
+                    OrderDetailsInfoViewModel.OrderActor(
+                        title: "Иванов Алексей Евгеньевич",
+                        icon: UIImage(named: "orderDetailInfo.userCircle")!,
+                        behavior: .copy
+                    ),
+                    OrderDetailsInfoViewModel.OrderActor(
+                        title: "+7 (000) 000-00-00",
+                        icon: UIImage(named: "orderDetailInfo.messageCircle")!,
+                        behavior: .contact
+                    )
+                ]
+            ),
+            .actor(
+                role: .receiver,
+                items: [
+                    OrderDetailsInfoViewModel.OrderActor(
+                        title: "Новосибирск, ул. Писарева 136, кв. 152",
+                        icon: UIImage(named: "orderDetailInfo.mapPin")!,
+                        behavior: .copy
+                    ),
+                    OrderDetailsInfoViewModel.OrderActor(
+                        title: "Иванов Сергей Алексеевич",
+                        icon: UIImage(named: "orderDetailInfo.userCircle")!,
+                        behavior: .copy
+                    ),
+                    OrderDetailsInfoViewModel.OrderActor(
+                        title: "+7 (000) 000-00-00",
+                        icon: UIImage(named: "orderDetailInfo.messageCircle")!,
+                        behavior: .contact
+                    )
+                ]
+            ),
+            .additionalServices(
+                services: [
+                    OrderDetailsInfoViewModel.AdditionalService(
+                        title: "Страхование",
+                        description: ["Объявленная стоимость: 1 000 ₽"]
+                    ),
+                    OrderDetailsInfoViewModel.AdditionalService(
+                        title: "Коробка",
+                        description: ["10 кг. 40×35×28 см. / 10 шт.", "30 кг. 69×39×42 см. / 5 шт."]
+                    ),
+                    OrderDetailsInfoViewModel.AdditionalService(
+                        title: "Доп. упаковочные материалы",
+                        description: ["Макулатурная бумага / 10 м."]
+                    )
+                ]
+            ),
+            .parcelInfo(
+                items: [
+                    OrderDetailsInfoViewModel.ParcelInfo(
+                        title: "Характер груза",
+                        description: "Бытовая техника, обувь",
+                        additionalInfo: nil,
+                        type: .default(nestedItems: [])
+                    ),
+                    OrderDetailsInfoViewModel.ParcelInfo(
+                        title: "Габариты места 1 (ДхШхВ)",
+                        description: "48×42×42 см.",
+                        additionalInfo: nil,
+                        type: .default(nestedItems: [
+                            OrderDetailsInfoViewModel.ParcelInfo(
+                                title: "Артикул: 435342112",
+                                description: "Майка Adidas Fusion Sport Max II / 2 шт.",
+                                additionalInfo: nil,
+                                type: .nested
+                            ),
+                            OrderDetailsInfoViewModel.ParcelInfo(
+                                title: "Артикул: 955342111",
+                                description: "Баскетбольный мяч Nike Classic / 1 шт.",
+                                additionalInfo: nil,
+                                type: .nested
+                            )
+                        ])
+                    ),
+                    OrderDetailsInfoViewModel.ParcelInfo(
+                        title: "Физический вес",
+                        description: "9,8 кг",
+                        additionalInfo: "Физический вес. Дополнительное описание для модалки",
+                        type: .default(nestedItems: [])
+                    )
+                ]
+            )
+        ]
+    )
 }

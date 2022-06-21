d4572a91ff6a8f005f535c2bd1a369d1ce4892b0
Alesya Volosach <alesya.volosach@gmail.com>
2022-06-21 10:55:59 +0300

    refact: Корректировки в структуре проекта

---
+++ DeliveryStatus/OrderDetailInfo/OrderDetailInfoWidgetViewModelBuilder.swift
@@ -0,0 +1,10 @@
+//
+//  OrderDetailInfoWidgetViewModelBuilder.swift
+//  DeliveryStatus
+//
+//  Created by Alesya Volosach on 21.06.2022.
+//
+
+import Foundation
+
+class OrderDetailInfoWidgetViewModelBuilder {}
